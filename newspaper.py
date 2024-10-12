393e6212a79159e4db6a4cab9583accef4c236a2
jadanielson <justin@jdanielson.us>
2024-10-12 15:23:57 -0500

    Add weather section

--- newspaper.py
+++ newspaper.py
@@ -37,15 +37,79 @@ iw.boldface(False)
 
 iw.linefeed(2)
 
+def sectionheader(section: str):
+    iw.boldface(True)
+    iw.printchar(b'\xd6\x20')
+    iw.printstr(section)
+    iw.carriagereturn()
+    iw.linefeed()
+
+    iw.repeatchar(b'\xd6', section.__len__()+2)
+    iw.carriagereturn()
+    iw.linefeed()
+    iw.boldface(True)
+
 ##### Weather Section
 
-iw.boldface(True)
-iw.printchar(b'\xd6')
-iw.printstr('Weather')
-iw.carriagereturn()
-iw.linefeed()
+weather_codes = { 
+    0: "Clear",
+    1: "Mostly Clear",
+    2: "Partly Cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Fog",
+    51: "Light Drizzle",
+    53: "Drizzle",
+    55: "Heavy Drizzle",
+    56: "Freezing Drizzle",
+    57: "Freezing Drizzle",
+    61: "Light Rain",
+    63: "Rain",
+    65: "Heavy Rain",
+    66: "Light Freezing Rain",
+    67: "Freezing Rain",
+    71: "Light Snow",
+    73: "Snow",
+    75: "Heavy Snow",
+    77: "Snow",
+    80: "Showers",
+    81: "Showers",
+    82: "Showers",
+    85: "Snow Showers",
+    86: "Snow Showers",
+    95: "Thunderstorm",
+    96: "Thunderstorm",
+    99: "Thunderstorm" }
 
-iw.repeatchar(b'\xd6',8)
+# Weather Header
+sectionheader("Weather")
+
+import openmeteo_requests
+import pandas as pd
+
+openmeteo = openmeteo_requests.Client()
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 43.9441,
+	"longitude": -90.8129,
+	"current": ["temperature_2m", "weather_code"],
+	"hourly": ["temperature_2m", "weather_code"],
+	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset"],
+	"wind_speed_unit": "kn",
+	"precipitation_unit": "inch",
+	"timezone": "America/Chicago",
+	"temporal_resolution": "hourly_3"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+response = responses[0]
+
+current = response.Current()
+current_temp = current.Variables(0).Value()
+current_weather_code = current.Variables(1).Value()
+
+currentstr = "Current: " + current_temp + "c " + weather_codes[current_weather_code]
+iw.printstr(currentstr)
 iw.carriagereturn()
 iw.linefeed()
-iw.boldface(True)
