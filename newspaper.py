4922c8a2290d1ffb1a65f49774aa2089f4ed7f7a
Justin Danielson <justin@jdanielson.us>
2024-10-12 14:38:53 -0500

    add newspaper.py

---
+++ newspaper.py
@@ -0,0 +1,46 @@
+import imagewriter
+from datetime import date
+import calendar
+
+iw = imagewriter.Imagewriter('/dev/ttyS0')
+
+##### Header
+
+dayofweek = calendar.day_name[date.today().weekday()]
+title = "Justin's News"
+datestring = date.today().strftime("%d %B %Y")
+leftspaces = 28-(3+dayofweek.__len__())
+rightspaces = 80-52-(1+datestring.__len__())
+
+iw.repeatchar(b'\xd6',80)
+iw.carriagereturn()
+iw.linefeed()
+
+iw.printchar(b'\xd6')
+iw.printstr(dayofweek)
+iw.repeatchar(b'\x20',leftspaces)
+iw.doublewidth(True)
+iw.printstr(title)
+iw.doublewidth(False)
+iw.repeatchar(b'\x20',rightspaces)
+iw.printstr(datestring)
+iw.printchar(b'\xd6')
+iw.carriagereturn()
+iw.linefeed()
+
+iw.repeatchar(b'\xd6',80)
+iw.carriagereturn()
+iw.linefeed()
+
+iw.linefeed(2)
+
+##### Weather Section
+
+iw.printchar(b'\xd6')
+iw.printstr('Weather')
+iw.carriagereturn()
+iw.linefeed()
+
+iw.repeatchar(b'\xd6',8)
+iw.carriagereturn()
+iw.linefeed()
